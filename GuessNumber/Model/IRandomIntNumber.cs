18bd96e8ac0e2abe1b4a61003e9e2207ad96e8be
agiledon <zhangyi1101@tom.com>
2019-09-04 13:37:28 +0800

    implements RandomIntNumber

--- GuessNumber/Model/IRandomIntNumber.cs
+++ GuessNumber/Model/IRandomIntNumber.cs
@@ -1,7 +1,19 @@
-﻿namespace GuessNumber.Model
+﻿using System;
+
+namespace GuessNumber.Model
 {
     public interface IRandomIntNumber
     {
         int Next();
+    }
+
+    public class RandomIntNumber : IRandomIntNumber
+    {
+        private readonly Random _random = new Random();
+
+        public int Next()
+        {
+            return _random.Next(0, 9);
+        }
     }
 }
